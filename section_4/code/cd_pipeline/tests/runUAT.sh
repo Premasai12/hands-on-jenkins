b9ca3aea2c6b53a89868ad9ad68b5a28e02cf5fe
Premasai12 <55290308+Premasai12@users.noreply.github.com>
2024-02-29 18:19:57 +0530

    Update runUAT.sh

--- section_4/code/cd_pipeline/tests/runUAT.sh
+++ section_4/code/cd_pipeline/tests/runUAT.sh
@@ -6,7 +6,7 @@ hostname='10.1.3.69'
 port=$1
 
 # wait for the app to start
-sleep 5 
+sleep 10 
 
 # ping the app
 status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
